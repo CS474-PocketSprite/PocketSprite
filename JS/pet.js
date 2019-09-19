755b6406b46b9c532aae77a2c94279244b322f7f
Gabe <gabecorella@wifi-roaming-128-4-137-131.host.udel.edu>
2019-09-19 10:19:29 -0400

    committing work...

--- JS/pet.js
+++ JS/pet.js
@@ -1,20 +1,11 @@
 
-function startScreen(){
-    document.getElementById("screenSaverBackground").click();
-    console.log("Button Clicked...");
+function getWidth(){
+    var width = Screen.width;
+}
+function inputName(){
+    var startButton =  document.getElementById("inputStart");
+    this.style.display = 'absolute';
+    
 }
 
-function onStart(){
-    document.getElementById('startButton').onclick = function(){
-         
-    }
-}
-// Create a Canvas
-function initCanvas(){
-    var x = document.createElement("CANVAS");
-    var ctx = x.getContext("2d");
 
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(20, 20, 150, 100);   
-    document.appendChild(x);
-}
