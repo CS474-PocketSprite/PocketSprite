ed26d422c0901c4e87e02fee247cc558ca302b4f
Betty Zhang <bettyzhang@wifi-roaming-128-4-109-23.host.udel.edu>
2019-09-23 12:56:53 -0400

    fixed some typos

--- JS/gameIndex.js
+++ JS/gameIndex.js
@@ -1,17 +1,17 @@
 const btn = document.querySelector('actionButton2');
 btn.addEventListener('click', (e)=> {
         e.preventDefault();
-	let height = document.querySelector('healthBar').style.height;
-	if(height == 250){
-	    if(document.querySelector('healthBar').style.background = rgb(225,0, 0)) {
-			document.querySelector('healthBar').style.height=50;
-			document.querySelector('healthBar').style.background = rgb(0,128, 0);
+	var height = document.getElementById("healthBar").height;
+	if(height == "250"){
+	    if(document.getElementById("healthBar").background = rgb(225,0, 0)) {
+		document.getElementById("healthBar").height="50";
+		document.getElementById("healthBar").background = rgb(0,128, 0);
 		}
 		else
 		document.querySelector('welcomeScreen').textContent = "You win!";
 	}
 	else{
-		document.querySelector('healthBar').style.height=50+height;
+		document.getElementById("healthBar").height="${50+height}";
 	}
 	// if height = 250 1. if background-color = rgb(225, 0, 0), change to green and update height to 50; 2. if color is green, change the title to "you win"
 	// else, get the previous height and increment it by 50
