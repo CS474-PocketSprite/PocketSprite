698366a2b6c0096276a6eeb238f69aaed083bb30
Betty Zhang <bettyzhang@Bettys-MacBook-Air.local>
2019-10-01 12:19:16 -0400

    applied changes to button event listener

--- JS/gameIndex.js
+++ JS/gameIndex.js
@@ -1,19 +1,22 @@
-const btn = document.querySelector('actionButton2');
-btn.addEventListener('click', (e)=> {
-        e.preventDefault();
-	var height = document.getElementById("healthBar").height;
-	if(height == "250"){
-	    if(document.getElementById("healthBar").background = rgb(225,0, 0)) {
-		document.getElementById("healthBar").height="50";
-		document.getElementById("healthBar").background = rgb(0,128, 0);
+var btn = document.getElementsByClassName('button-group');
+for (var i = 0; i < btn.length; i++){
+    console.log("button is"+i);
+    if(btn[i]){
+	btn[i].addEventListener('click', (e)=> {
+		e.preventDefault();
+		console.log("button clicke");
+		var height = document.getElementById("healthBar").height;
+		if(height == "250"){
+		    if(document.getElementById("healthBar").background = rgb(225,0, 0)) {
+			document.getElementById("healthBar").height="50";
+			document.getElementById("healthBar").background = rgb(0,128, 0);
+		    }
+		    else
+			document.querySelector('welcomeScreen').textContent = "You win!";
 		}
-		else
-		document.querySelector('welcomeScreen').textContent = "You win!";
-	}
-	else{
-		document.getElementById("healthBar").height="${50+height}";
-	}
-	// if height = 250 1. if background-color = rgb(225, 0, 0), change to green and update height to 50; 2. if color is green, change the title to "you win"
-	// else, get the previous height and increment it by 50
-	//    document.querySelector(‘healthBar’).style.background = ‘#ccc’;
-});
+		else{
+		    document.getElementById("healthBar").height="${50+height}";
+		}
+	    });
+    }
+}
